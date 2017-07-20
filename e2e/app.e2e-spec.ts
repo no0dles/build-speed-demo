d095058a798a2007729698d19a9d8b1603d68bdf
Angular CLI <angular-cli@angular.io>
2017-07-20 19:49:23 +0200

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { BuildSpeedDemoPage } from './app.po';
+
+describe('build-speed-demo App', () => {
+  let page: BuildSpeedDemoPage;
+
+  beforeEach(() => {
+    page = new BuildSpeedDemoPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
