66d783c6e7d3fc3f78ff66ac3277bd644e54d051
Pascal Bertschi <pascal.bertschi@hotmail.com>
2017-07-22 16:34:06 +0200

    drone config

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('build-speed-demo App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to app build!');
   });
 });
